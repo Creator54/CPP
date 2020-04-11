00e58e68661015d23060162b7407852e216f4299
Creator54 <hi.creator54@gmail.com>
2020-04-11 17:40:23 +0530

    input in cpp
    Signed-off-by: Creator54 <hi.creator54@gmail.com>

---
+++ general/input.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+  int a;
+  string name;
+  cout << "Enter an integer ";
+  cin >> a;
+  //takes an integer input similar for any input type
+  cout << "Your input " << a << endl;
+  cout << "Enter your name ";
+  cin >> name;
+  cout << "Your input " << name ;
+
+  return 0;
+}
