67d75360bb6e4f188dc0da99bc1891b1d22371ff
Creator54 <hi.creator54@gmail.com>
2020-06-09 00:06:34 +0530

    bottom
    Signed-off-by: Creator54 <hi.creator54@gmail.com>

--- general/star.cpp
+++ general/star.cpp
@@ -1,26 +1,43 @@
 #include<iostream>
 using namespace std;
 
-int h;
+int h,count=0,c1=0;
 char x;
 void top(int mid){
 	for(int i=0;i<h;i++){
-		for(int j=0;j<mid;j++) cout<<" ";
+		for(int j=0;j<mid;j++)
+			cout<<" ";
 		if(i==0)cout<<x<<endl;
 		else{
+			count++;
 			cout<<x;
-			for(int j=0;j!=2*i-1;j++) cout<<" ";
+			for(int j=0;j!=2*i-1;j++){
+				cout<<" ";
+				if(i==h-1)c1++;
+			}
 			cout<<x<<endl;
 		}
 		mid--;
 	}
+	count=mid;
 	for(int i=0;i<(3*h+1);i++) cout<<x<<" ";
-	cout<<endl;
+	cout<<endl<<endl;
 }
 void middle(int mid){
+
 }
 void bottom(int mid){
-
+	for(int i=0;i<(3*h+1);i++) cout<<x<<" ";
+	cout<<endl;
+	for(int i=0;i<h;i++){
+		for(int j=0;j<count+1;j++) cout<<" ";
+		cout<<x;
+		count++;
+		for(int j=0;j<c1;j++) cout<<" ";
+		if(i!=h-1) cout<<x<<endl;
+		c1-=2;
+	}
+	cout<<endl;
 }
 void star(){
 	system("clear");
