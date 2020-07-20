4e477d21c969d4eac0b6ac257579be1146cae7f5
Creator54 <hi.creator54@gmail.com>
2020-07-20 12:04:37 +0530

    fix had issues with inputs like  1 2 3 6 6 ,3 6 1 2 6

--- general/array_division.cpp
+++ general/array_division.cpp
@@ -7,16 +7,26 @@ Try it!
 #include<iostream>
 #define f(i,a,b) for(int i=a;i<b;i++)
 
+int size;
+int *sort(int arr[]){
+	f(i,0,size-1){
+		int tmp,smallest=arr[i],s_index=i;
+		f(j,i+1,size)if(smallest>arr[j]) smallest=arr[j],s_index=j;
+		if(s_index!=i) tmp =arr[i],arr[i]=smallest,arr[s_index]=tmp;
+	}
+	return arr;
+}
+
 int main(){
-	int size,sum=0,s1=0,s2=0,f1=1;
+	int sum=0,s1=0,s2=0;
 	std::cin>>size;
 	int arr[size];
 	f(i,0,size) std::cin>>arr[i],sum+=arr[i];
+	sort(arr);
 	if(sum%3==0){
 		f(i,0,size){
-			if(f1!=0)s1+=arr[i];
-			if(s1==sum/3){
-				f1=0;
+			if(s1!=sum/3)s1+=arr[i];
+			else{
 				if(s2<sum/3){
 					s2+=arr[i];
 					if(s2==sum/3){
